7dec83143342529b3ac78823c676ca300ccceec8
Levente Polyak <levente@leventepolyak.net>
2020-10-15 00:07:30 +0200

    section: adding collapsible elements including good packages

--- src/Section.js
+++ src/Section.js
@@ -1,24 +1,41 @@
 'use strict';
 
 const React = require('react');
+import Collapsible from 'react-collapsible'
 
+function StatusSection(props) {
+  const isOpen = props.open;
+  const content = (
+    <ul>
+    {props.pkgs.map(function(pkg) {
+      return <li key={pkg.name}><p className="subtitle is-6 has-text-white">{pkg.name} {pkg.version}</p></li>
+    })}
+    </ul>
+  );
+  return (
+    <div>
+    { isOpen
+      ? <Collapsible trigger={props.label} lazyRender open>{ content }</Collapsible>
+      : <Collapsible trigger={props.label} lazyRender>{ content }</Collapsible>
+    }
+    </div>
+  );
+}
 
 class Section extends React.Component {
   render() {
     const { suite } = this.props;
+    const good = suite.pkgs.filter(pkg => pkg.status == "GOOD");
+    const bad = suite.pkgs.filter(pkg => pkg.status == "BAD");
+    const unknown = suite.pkgs.filter(pkg => pkg.status == "UNKWN");
     return (
-      <section key={suite.name} className="section pt-4 pb-4" >
+      <section key={suite.name} className="section pt-4 pb-4" id={ suite.name }>
         <div className="tile box has-background-danger">
-          <div className="content">
-            <p className='title is-5 has-text-white'>{ suite.name }</p>
-              <ul>
-              {suite.pkgs.map(function(pkg) { 
-                if (pkg.status == 'BAD') {
-                  return <li key={pkg.name}><p className="subtitle is-6 has-text-white">{pkg.name}-{pkg.version}</p></li>
-                }
-              })}
-              </ul>
-          </div>
+          <Collapsible trigger={ suite.name } open>
+            {good.length > 0 && <StatusSection label="good" pkgs={ good } />}
+            {bad.length > 0 && <StatusSection label="bad" pkgs={ bad } open />}
+            {unknown.length > 0 && <StatusSection label="unknown" pkgs={ unknown } open />}
+          </Collapsible>
         </div>
       </section>
     )
